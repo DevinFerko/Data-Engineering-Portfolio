68571b20f044a67eaa88b56aaebfc0da674b9c5c
Devin Ferko <devin.ferko@outlook.com>
2026-03-10 09:05:32 +0000

    Fetch Function, Normailise jso function, read config function, boilerplate comments

--- Projects/Modern Data Pipeline/Data Ingestion/extract_api.py
+++ Projects/Modern Data Pipeline/Data Ingestion/extract_api.py
@@ -13,8 +13,18 @@ api_base = "https://fakestoreapi.com"
 headers = {"Content-Type": "application/json"}
 
 # BigQuery
-bq_project = 'Insert'
-bq_dataset = 'Insert'
+config = {}
+
+with open(r"C:\Users\Devin Ferko\Desktop\Codes\Data Engineering Portfolio\Projects\Modern Data Pipeline\Data Ingestion\local_config.txt") as f:
+    for line in f:
+        key, value = line.strip().split("=")
+        config[key] = value
+
+bq_project = config["bq_project"]
+bq_dataset = config["bq_dataset"]
+
+print(bq_project)
+print(bq_dataset)
 
 # Map endpoints to Table Names
 tables = {
@@ -28,3 +38,43 @@ client = bigquery.Client()
 # -------------
 # API Fetch Function
 # -------------
+
+def fetch_api(endpoint):
+    url = f"{api_base}/{endpoint}"
+    response = requests.get(url, headers=headers)
+    response.raise_for_status() #fails if api is down
+    return response.json()
+
+# -------------
+# Normalize json
+# -------------
+
+def normalize_json(data):
+    df = pd.json_normalize(data)
+    df.columns = df.columns.str.replace(".", "_") #flattens columns names
+    return df
+
+# -------------
+# Saves Raw json
+# -------------
+
+
+# -------------
+# Loads df to BigQuery
+# -------------
+
+
+# -------------
+# Fetch/load per table
+# -------------
+
+
+# -------------
+# Main
+# -------------
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
