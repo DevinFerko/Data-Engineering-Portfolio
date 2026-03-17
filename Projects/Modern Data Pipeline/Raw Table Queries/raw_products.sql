ed91471da7560f778e541d8d78d5d2bb96b50a55
Devin Ferko <devin.ferko@outlook.com>
2026-03-17 09:33:08 +0000

    Create/Replace table query

---
+++ Projects/Modern Data Pipeline/Raw Table Queries/raw_products.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE TABLE `your_project_id.raw.products` (
+    id INT64,
+    title STRING,
+    price FLOAT64,
+    description STRING,
+    category STRING,
+    image STRING,
+    rating_rate FLOAT64,
+    rating_count INT64
+);
